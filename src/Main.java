09aec92ec382a7128741db637d91c4a36c6c2fff
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-01-31 22:30:48 -0300

    Criado retornos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-    Quadrilatero.area(6);
-    Quadrilatero.area(12D, 8D);
-    Quadrilatero.area(9D, 4D, 2D);
-    Quadrilatero.area(5F, 12F);
-
+        // retornos
+        System.out.println(Quadrilatero2.area(5));
+        System.out.println(Quadrilatero2.area(6, 2));
+        System.out.println(Quadrilatero2.area(4, 8 , 12));
     }
 }
